4529db4c00995d9e4b02ec599f4ad6211f9afb17
beasthunter758 <53615098+beasthunter758@users.noreply.github.com>
2019-10-05 12:28:08 +0530

    Add files via upload

---
+++ Web dev/HTML/practice.js
@@ -0,0 +1,11 @@
+var date=new Date();  
+var day=date.getDate();  
+var month=date.getMonth()+1;  
+var year=date.getFullYear();  
+document.write("<br>Date is: "+day+"/"+month+"/"+year);  
+str1=new String("Welcome");
+str2=new String("to");
+str3=new String("XUB")
+
+alert(str1+str2+str3);
+
